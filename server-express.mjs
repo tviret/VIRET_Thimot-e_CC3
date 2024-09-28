f6c105b6dc27668d02991408050225b86a7bd6fe
tviret <thimoteeviret@gmail.com>
2024-09-28 11:13:44 +1100

    Ajout de la gestion d'erreur avec http-errors

--- server-express.mjs
+++ server-express.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import createError from "http-errors";  
 
 const host = "localhost";
 const port = 8000;
 
 const app = express();
+//app.use(http_errors)
 
 app.set("view engine", "ejs");
 
@@ -13,7 +15,11 @@ app.use(express.static("static", {index:"index.html"}));
 if (app.get("env") === "development") app.use(morgan("dev"));
 
 app.get("/random/:nb", async function (request, response, next) {
-  const length = request.params.nb;
+
+  const length = Number.parseInt(request.params.nb, 10);
+
+  if(Number.isNaN(length)) { return next(createError(400, "Le paramètre n'est pas un nombre valide"));}; // on vérifie que l'argument est bien un nombre
+  
   const numbers = Array.from({ length},()=>Math.floor(Math.random()*100))
   const welcome = "Bienvenue sur la page de nombres aléatoires"
 
